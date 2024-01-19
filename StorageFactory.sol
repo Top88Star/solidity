291c702b1cc97e6c2b23863a0084e9f25b83afa9
Top88Star <70pstar@gmail.com>
2024-01-19 09:20:03 +0200

    2nd contract

---
+++ StorageFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory{
+
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 ) public {
+
+    }
+}
